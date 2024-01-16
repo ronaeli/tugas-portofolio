2c78a0a975e943d3a76bf65d18863f0dd50161a2
ronaeli <73942800+ronaeli@users.noreply.github.com>
2024-01-16 22:51:28 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' }); // Menggunakan window.scrollTo untuk mengatur posisi scroll ke atas halaman dengan efek smooth
+  }
+
